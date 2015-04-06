abd148b57f0deee4ae85962dd1f909f0e28760b5
vallejo314 <novi.singh94@gmail.com>
2015-04-05 21:29:14 -0500

    Reworked secret key flow
    Reworked the key flow to be more in line with what our actual HDFS
    changes will be.

--- simulate_request.py
+++ simulate_request.py
@@ -16,58 +16,57 @@ KMS_NUM = 10
 
 TCP_IP = '127.0.0.1'
 BUFFER_SIZE = 1024
+# Base port for User to listen for KMS replies
+USER_KMS_LISTEN_PORT = 5100
 # Base port for NameNode to listen for the User.
 NAMENODE_USER_LISTEN_PORT = 5200
 # Base port for KMS to listen for the NameNode
 KMS_NAMENODE_LISTEN_PORT = 5400
 
 # Code for the User
-def make_request(destination_port):
-	send_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	send_socket.connect((TCP_IP, destination_port))
-	send_socket.send('pls')
-
-	response = send_socket.recv(BUFFER_SIZE)
-	send_socket.close()
-
-	print "Response is: %s" % (response)
-	print "Actual is: %s" % (KEY)
-
-# Code for the NameNode
-def listen_for_user_request(user_listen_port, kms_ports):
-	
+def make_request(user_listen_port, destination_port):
 	# First set up the listening ports.
 	user_listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	user_listen_socket.bind((TCP_IP, user_listen_port))
 	user_listen_socket.listen(1)
 
-	# Get the user request
-	user_conn, user_addr = user_listen_socket.accept()
-	request = user_conn.recv(BUFFER_SIZE)
+	# Connect to the name node.
+	send_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	send_socket.connect((TCP_IP, destination_port))
+	send_socket.send('%s:%s' % (TCP_IP, user_listen_port))
 
-	print 'User request received.'
+	send_socket.close()
+
+	# Wait for a KMS socket to respond
+	received_shares = []
+	while len(received_shares) < M_NUM:
+		kms_conn, kms_addr = user_listen_socket.accept()
+		received_shares.append(kms_conn.recv(BUFFER_SIZE))
+		kms_conn.close()
+
+	# Reconstruct the key and send it back to the user
+	key = PlaintextToHexSecretSharer.recover_secret(received_shares)
+	print 'Key reconstructed.'	
+	print "Response is: %s" % (key)
+	print "Actual is: %s" % (KEY)
+
+# Code for the NameNode
+def listen_for_user_request(listen_port, kms_ports):
+	# Get the user request	# First set up the listening ports.
+	listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	listen_socket.bind((TCP_IP, listen_port))
+	listen_socket.listen(1)
+	
+	user_conn, user_addr = listen_socket.accept()
+	request = user_conn.recv(BUFFER_SIZE)
 
 	# Ask the KMS for key shares
 	kms_sockets = []
 	for kms_port in kms_ports:
 		kms_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		kms_socket.connect((TCP_IP, kms_port))
-		kms_socket.send('pls')
+		kms_socket.send(request)
 		kms_sockets.append(kms_socket)
-
-	# Wait for a kms socket to respond
-	received_shares = []
-	while len(received_shares) < M_NUM:
-		ready, _, _ = select.select(kms_sockets, [], [])
-		for kms_socket in ready:
-			if len(received_shares) >= M_NUM:
-				break
-			received_shares.append(kms_socket.recv(BUFFER_SIZE))
-
-	# Reconstruct the key and send it back to the user
-	key = PlaintextToHexSecretSharer.recover_secret(received_shares)
-	print 'Key reconstructed.'
-	user_conn.send(key)
 	
 	# Cleanup.
 	user_conn.close()
@@ -82,9 +81,17 @@ def listen_for_name_node_request(listen_port, share):
 	listen_socket.listen(1)
 
 	node_conn, node_addr = listen_socket.accept()
-	request = node_conn.recv(BUFFER_SIZE)
-	node_conn.send(share)
+	dest_addr = node_conn.recv(BUFFER_SIZE).split(':')
 	node_conn.close()
+
+	# Send share to user.
+	send_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	try:
+		send_socket.connect((dest_addr[0], int(dest_addr[1])))
+		send_socket.send(share)
+		send_socket.close()
+	except socket.error, msg:
+		pass
 
 class SetupProcesses:
     """
@@ -114,7 +121,7 @@ class SetupProcesses:
         # Spawn the User.
         user = multiprocessing.Process(
             target = make_request,
-            args = (NAMENODE_USER_LISTEN_PORT,))
+            args = (USER_KMS_LISTEN_PORT, NAMENODE_USER_LISTEN_PORT))
         user.start()
 
 
